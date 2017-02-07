e2f76ba989c884a9956f56ea97db5795efd7b0f5
JonataBecker <jonatabecker@gmail.com>
2017-02-06 22:37:26 -0200

    Created the binary labeling process

--- src/org/torax/pdi/BinaryLabelingProcess.java
+++ src/org/torax/pdi/BinaryLabelingProcess.java
@@ -4,9 +4,12 @@ import org.torax.commons.BinaryLabeling;
 import org.torax.commons.Image;
 
 /**
- * Banary labeling process
+ * Binary labeling process
  */
 public class BinaryLabelingProcess extends PixelProcess<BinaryLabeling> {
+
+    /** Initial label */
+    private static final int INITIAL_LABEL = 2;
 
     /** Output */
     private final Image out;
@@ -16,9 +19,9 @@ public class BinaryLabelingProcess extends PixelProcess<BinaryLabeling> {
     public BinaryLabelingProcess(Image image) {
         super(image);
         this.out = new Image(this.image);
-        this.indexLabel = 2;
+        this.indexLabel = INITIAL_LABEL;
         setFinalizer(() -> {
-            setOutput(new BinaryLabeling(out, indexLabel - 2));
+            setOutput(new BinaryLabeling(out, indexLabel - INITIAL_LABEL));
         });
     }
 
@@ -30,16 +33,53 @@ public class BinaryLabelingProcess extends PixelProcess<BinaryLabeling> {
     }
 
     /**
-     * Execute the label comparison 
-     * 
+     * Returns the label size
+     *
+     * @param label
+     * @return int
+     */
+    public int getSize(int label) {
+        int compareLabel = label + 1;
+        int size = 0;
+        for (int y = 0; y < out.getHeight(); y++) {
+            for (int x = 0; x < out.getWidth(); x++) {
+                if (out.get(0, x, y) == compareLabel) {
+                    size++;
+                }
+            }
+        }
+        return size;
+    }
+
+    /**
+     * Returns a binary matrix
+     *
+     * @param label
+     * @return boolean[][]
+     */
+    public boolean[][] getMatrix(int label) {
+        boolean[][] mat = new boolean[out.getWidth()][out.getHeight()];
+        int compareLabel = label + 1;
+        for (int y = 0; y < out.getHeight(); y++) {
+            for (int x = 0; x < out.getWidth(); x++) {
+                if (out.get(0, x, y) == compareLabel) {
+                    mat[x][y] = true;
+                }
+            }
+        }
+        return mat;
+    }
+
+    /**
+     * Execute the label comparison
+     *
      * @param channel
      * @param x
      * @param y
-     * @param index 
+     * @param index
      */
     private void compareLabel(int channel, int x, int y, int index) {
-        if (x < 0 || x >= out.getWidth() || 
-                y < 0 || y >= out.getHeight()) {
+        if (x < 0 || x >= out.getWidth() || y < 0 || y >= out.getHeight()) {
             return;
         }
         if (out.get(channel, x, y) == 1) {
--- test/org/torax/pdi/BinaryLabelingProcessTest.java
+++ test/org/torax/pdi/BinaryLabelingProcessTest.java
@@ -1,5 +1,9 @@
 package org.torax.pdi;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+import junit.framework.Assert;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.torax.commons.BinaryLabeling;
@@ -28,6 +32,45 @@ public class BinaryLabelingProcessTest {
         BinaryLabeling out = process.getOutput();
         ImageAssert.assertImage(expected, out.getImage());
         assertEquals(3, out.getSize());
+    }
+
+    /**
+     * Test the label size calculation
+     */
+    @Test
+    public void testLabelSize() {
+        BinaryLabelingProcess process = new BinaryLabelingProcess(buildTestImage());
+        process.process();
+        assertEquals(4, process.getSize(1));
+        assertEquals(7, process.getSize(2));
+        assertEquals(2, process.getSize(3));
+    }
+
+    /**
+     * Test the label size calculation
+     */
+    @Test
+    public void testMatrix() {
+        BinaryLabelingProcess process = new BinaryLabelingProcess(buildTestImage());
+        process.process();
+        boolean label1[][] = new boolean[][]{
+            {false, true, true, false, false, false, false, false, false, false},
+            {false, true, true, false, false, false, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false, false}
+        };
+        boolean label2[][] = new boolean[][]{
+            {false, false, false, false, false, false, false, true, false, true},
+            {false, false, false, false, false, false, false, true, true, true},
+            {false, false, false, false, false, false, false, true, false, true}
+        };
+        boolean label3[][] = new boolean[][]{
+            {false, false, false, false, false, false, false, false, false, false},
+            {false, false, false, false, true, true, false, false, false, false},
+            {false, false, false, false, false, false, false, false, false, false}
+        };
+        assertArrayEquals(label1, process.getMatrix(1));
+        assertArrayEquals(label2, process.getMatrix(2));
+        assertArrayEquals(label3, process.getMatrix(3));
     }
 
     /**
