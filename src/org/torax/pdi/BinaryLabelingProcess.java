918103bd6c9c73fd7be62b1cb5862a5ee41b21e5
Jouwee <nicolaspohren@gmail.com>
2017-08-07 22:49:49 -0300

    Created beans to store information about the extracted objets by binary labeling

--- src/org/torax/pdi/BinaryLabelingProcess.java
+++ src/org/torax/pdi/BinaryLabelingProcess.java
@@ -1,5 +1,10 @@
 package org.torax.pdi;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import org.torax.commons.BinaryLabeling;
 import org.torax.commons.Image;
 
@@ -10,12 +15,16 @@ public class BinaryLabelingProcess extends PixelProcess<BinaryLabeling> {
 
     /** Initial label */
     private static final int INITIAL_LABEL = 2;
-
     /** Output */
     private final Image out;
     /** Index label */
     private int indexLabel;
 
+    /**
+     * Creates a new BinaryLabeling process
+     * 
+     * @param image 
+     */
     public BinaryLabelingProcess(Image image) {
         super(image);
         this.out = new Image(this.image);
@@ -89,6 +98,114 @@ public class BinaryLabelingProcess extends PixelProcess<BinaryLabeling> {
             compareLabel(channel, x, y + 1, index);
             compareLabel(channel, x + 1, y, index);
         }
+    }
+    
+    /**
+     * Returns the objects extracted by this process
+     * 
+     * @return ObjectList
+     */
+    public ObjectList getExtractedObjects() {
+        Map<Integer, ExtractedObject> map = new HashMap<>();
+        for (int y = 0; y < out.getHeight(); y++) {
+            for (int x = 0; x < out.getWidth(); x++) {
+                int label = out.get(0, x, y);
+                if (label < INITIAL_LABEL) {
+                    continue;
+                }
+                ExtractedObject object = map.get(label);
+                if (object == null) {
+                    object = new ExtractedObject(label);
+                    map.put(label, object);
+                }
+                object.size++;
+                object.matrix[x][y] = true;
+            }
+        }
+        return new ObjectList(map.values());
+    }
+    
+    /**
+     * Object list identified by the process
+     */
+    public class ObjectList extends ArrayList<ExtractedObject> {
+
+        /**
+         * Creates a new Object list based on a collection
+         * @param collection 
+         */
+        public ObjectList(Collection<ExtractedObject> collection) {
+            super(collection);
+        }
+        
+        /**
+         * Sorts by size in decrescent order (Largest first)
+         */
+        public void sortBySizeLargestFirst() {
+            sort((ExtractedObject o1, ExtractedObject o2) -> {
+                return o2.size - o1.size;
+            });
+        }
+        
+    }
+    
+    /**
+     * Extracted object
+     */
+    public class ExtractedObject {
+        
+        /** Label of this object */
+        private final int label;
+        /** Matrix of this object */
+        private final boolean[][] matrix;
+        /** Size */
+        private int size;
+
+        /**
+         * Creates a new extracted object
+         * 
+         * @param label
+         */
+        public ExtractedObject(int label) {
+            this.label = label;
+            this.matrix = new boolean[out.getWidth()][out.getHeight()];
+            this.size = 0;
+        }
+
+        /**
+         * Returns the label of this object
+         * 
+         * @return int
+         */
+        public int getLabel() {
+            return label;
+        }
+
+        /**
+         * Returns the matrix of this object
+         * 
+         * @return matrix
+         */
+        public boolean[][] getMatrix() {
+            return matrix;
+        }
+
+        /**
+         * Returns the size of this object (Area)
+         * 
+         * @return int
+         */
+        public int getSize() {
+            return size;
+        }
+
+        @Override
+        public String toString() {
+            return "ExtractedObject{" + "label=" + label + ", matrix=" + matrix + ", size=" + size + '}';
+        }
+
+        
+        
     }
 
 }
