0c408dd943ef4f07d0657664ce7453ad36df55d7
Jouwee <nicolaspohren@gmail.com>
2017-09-12 21:58:11 -0300

    Created mean, mode and median blur

---
+++ src/main/java/org/paim/pdi/MedianBlurProcess.java
@@ -0,0 +1,50 @@
+package org.paim.pdi;
+
+import org.paim.commons.Image;
+
+/**
+ * Blur using the Median of the values
+ */
+public class MedianBlurProcess  extends SimpleConvolutionProcess {
+
+    /**
+     * Creates a new Blur using the Median of the values process
+     *
+     * @param image
+     */
+    public MedianBlurProcess(Image image) {
+        super(image);
+    }
+
+    @Override
+    protected int computeCenter(int[][] neighbours) {
+        int[] values = new int[9];
+        for (int x = 0; x < neighbours.length; x++) {
+            for (int y = 0; y < neighbours[x].length; y++) {
+                int value = neighbours[x][y];
+                insert(values, value);
+            }
+        }
+        return values[values.length / 2];
+    }
+    
+    /**
+     * Insert a value in a ordered array
+     * 
+     * @param values
+     * @param newValue 
+     */
+    private void insert(int[] values, int newValue) {
+        for (int i = 0; i < values.length; i++) {
+            // If the number should go in the current position
+            if (i == values.length - 1 ||  newValue > values[i]) {
+                // Relocate
+                for (int j = values.length - 2; j >= i; j--) {
+                    values[j + 1] = values[j];
+                }
+                values[i] = newValue;
+            }
+        }
+    }
+
+}
