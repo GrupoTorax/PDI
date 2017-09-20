135e5cd30bb93058da8adf8e25bf4ec5be991f97
Jouwee <nicolaspohren@gmail.com>
2017-09-20 18:32:56 -0300

    Fixed convolution processes to have the same behavior as ImageJ for out-of-image pixels, which is considering the closes neighbor

--- src/main/java/org/paim/pdi/SimpleConvolutionProcess.java
+++ src/main/java/org/paim/pdi/SimpleConvolutionProcess.java
@@ -27,12 +27,18 @@ public abstract class SimpleConvolutionProcess extends ImageProcess<Image> {
     @Override
     protected void processImage() {
         for (int channel = 0; channel < image.getChannelCount(); channel++) {
-            for (int x = 1; x < image.getWidth() - 1; x++) {
-                for (int y = 1; y < image.getHeight() - 1; y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
                     int[][] neighbours = new int[3][3];
                     for (int lx = 0; lx < 3; lx++) {
                         for (int ly = 0; ly < 3; ly++) {
-                            neighbours[lx][ly] = image.get(channel, x + lx - 1, y + ly - 1);
+                            int ix = x + lx - 1;
+                            int iy = y + ly - 1;
+                            if (ix < 0 || iy < 0 || ix >= image.getWidth() || iy >= image.getHeight()) {
+                                neighbours[lx][ly] = getNearestNeighbour(channel, ix, iy);
+                            } else {
+                                neighbours[lx][ly] = image.get(channel, ix, iy);
+                            }
                         }
                     }
                     int newValue = computeCenter(neighbours);
@@ -40,6 +46,30 @@ public abstract class SimpleConvolutionProcess extends ImageProcess<Image> {
                 }
             }
         }
+    }
+    
+    /**
+     * Returns the nearest neighbour
+     * 
+     * @param channel
+     * @param x
+     * @param y
+     * @return int
+     */
+    private int getNearestNeighbour(int channel, int x, int y) {
+        if (x < 0) {
+            x = 0;
+        }
+        if (y < 0) {
+            y = 0;
+        }
+        if (x >= image.getWidth()) {
+            x = image.getWidth() - 1;
+        }
+        if (y >= image.getHeight()) {
+            y = image.getHeight() - 1;
+        }
+        return image.get(channel, x, y);
     }
 
     protected abstract int computeCenter(int[][] neighbours);
