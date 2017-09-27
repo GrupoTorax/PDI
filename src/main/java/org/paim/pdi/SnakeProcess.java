aae0b85851f4f07a09c708c873a2c268f1371762
JonataBecker <jonatabecker@gmail.com>
2017-09-27 20:30:01 -0300

    Created snake process

--- src/main/java/org/paim/pdi/SnakeProcess.java
+++ src/main/java/org/paim/pdi/SnakeProcess.java
@@ -14,7 +14,7 @@ import org.paim.commons.Point;
 public class SnakeProcess extends ImageProcess<BinaryImage> {
 
     private final int MAXLEN = 16;
-    private final int THRESHOLD = 100;
+    private final int THRESHOLD = 25;
 
     private final BinaryImage binaryImage;
     private final int steps;
@@ -22,15 +22,13 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
     private final int beta;
     private final int gamma;
     private final int delta;
-
-    private double[][] e_uniformity = new double[3][3];
-    private double[][] e_curvature = new double[3][3];
-    private double[][] e_flow = new double[3][3];
-    private double[][] e_inertia = new double[3][3];
-
+    private final double[][] energyUniformity = new double[3][3];
+    private final double[][] energyCurvature = new double[3][3];
+    private final double[][] energyFlow = new double[3][3];
+    private final double[][] energyInertia = new double[3][3];
     private int[][] gradient;
     private int[][] flow;
-    
+
     public SnakeProcess(Image image, int steps, int alpha, int beta, int gamma) {
         super(image);
         this.steps = steps;
@@ -47,10 +45,7 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
     @Override
     protected void processImage() {
         List<Point> points = buildInitialPoints();
-
         buildGradient();
-        
-
         for (int i = 0; i < steps; i++) {
             List newPoints = step(points, gradient, flow);
             if (newPoints == null) {
@@ -61,6 +56,11 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
         buildBinaryImage(points);
     }
 
+    /**
+     * Creates a binary imagen from a list of points
+     * 
+     * @param points 
+     */
     private void buildBinaryImage(List<Point> points) {
         int[] xPoints = points.stream().map((it) -> {
             return it.x;
@@ -77,30 +77,34 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
             }
         }
     }
-
-    private void  buildGradient() {
-        int W = image.getWidth();
-        int H = image.getHeight();
-
-        int[][] clum = new int[W][H];
-        for (int y = 0; y < H; y++) {
-            for (int x = 0; x < W; x++) {
-                if (image.isGrayScale()) {
-                    clum[x][y] = image.get(Image.CHANNELS_GRAYSCALE, x, y);
+    
+    /**
+     * Builds de gradient/flow matrix
+     */
+    private void buildGradient() {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int[][] clum = new int[width][height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (image.getPixelValueRange().isBinary()) {
+                    clum[x][y] = image.get(Image.CHANNEL_GRAY, x, y) == 1 ? 255 : 0;
                 } else {
-
-                    int r = image.get(Image.CHANNEL_RED, x, y);
-                    int g = image.get(Image.CHANNEL_GREEN, x, y);
-                    int b = image.get(Image.CHANNEL_BLUE, x, y);
-                    clum[x][y] = ((int) (0.299D * r + 0.587D * g + 0.114D * b));
+                    if (image.isGrayScale()) {
+                        clum[x][y] = image.get(Image.CHANNEL_GRAY, x, y);
+                    } else {
+                        int r = image.get(Image.CHANNEL_RED, x, y);
+                        int g = image.get(Image.CHANNEL_GREEN, x, y);
+                        int b = image.get(Image.CHANNEL_BLUE, x, y);
+                        clum[x][y] = ((int) (0.299D * r + 0.587D * g + 0.114D * b));
+                    }
                 }
             }
         }
-
         gradient = new int[image.getWidth()][image.getHeight()];
         int maxgradient = 0;
-        for (int y = 0; y < H - 2; y++) {
-            for (int x = 0; x < W - 2; x++) {
+        for (int y = 0; y < height - 2; y++) {
+            for (int x = 0; x < width - 2; x++) {
                 int p00 = clum[(x + 0)][(y + 0)];
                 int p10 = clum[(x + 1)][(y + 0)];
                 int p20 = clum[(x + 2)][(y + 0)];
@@ -116,10 +120,9 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
                 maxgradient = Math.max(maxgradient, snorm);
             }
         }
-
-        boolean[][] binarygradient = new boolean[W][H];
-        for (int y = 0; y < H; y++) {
-            for (int x = 0; x < W; x++) {
+        boolean[][] binarygradient = new boolean[width][height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
                 if (gradient[x][y] > THRESHOLD * maxgradient / 100) {
                     binarygradient[x][y] = true;
                 } else {
@@ -127,21 +130,23 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
                 }
             }
         }
-
-       flow = new int[W][H];
-        double[][] cdist = new ChamferDistance(ChamferDistance.chamfer5).compute(binarygradient, W, H);
-        for (int y = 0; y < H; y++) {
-          for (int x = 0; x < W; x++) {
-            flow[x][y] = ((int)(5.0D * cdist[x][y]));
-          }
+        flow = new int[width][height];
+        double[][] cdist = new ChamferDistance().compute(binarygradient, width, height);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                flow[x][y] = ((int) (5.0D * cdist[x][y]));
+            }
         }
     }
-
+    
+    /**
+     * Builds the snake initial points 
+     * 
+     * @return {@code List<Point>}
+     */
     private List<Point> buildInitialPoints() {
-
         double halfWidth = image.getWidth() / 2;
         double halfHeight = image.getHeight() / 2;
-
         double radius = (halfWidth + halfHeight) / 2;
         double perimeter = 6.28D * radius;
         int nmb = (int) (perimeter / MAXLEN);
@@ -154,6 +159,14 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
         return circle;
     }
 
+    /**
+     * Execute a point search, if the snake don't change, the method returns null
+     * 
+     * @param points
+     * @param gradient
+     * @param flow
+     * @return {@code List<Point>}
+     */
     private List<Point> step(List<Point> points, int[][] gradient, int[][] flow) {
         List newPoints = new ArrayList(points.size());
         boolean changed = false;
@@ -162,44 +175,34 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
             Point prev = points.get((i + points.size() - 1) % points.size());
             Point cur = points.get(i);
             Point next = points.get((i + 1) % points.size());
-
-            int dy = -1;
-            while (dy <= 1) {
-                int dx = -1;
-                while (dx <= 1) {
+            for (int dy = -1; dy < 2; dy++) {
+                for (int dx = -1; dx < 2; dx++) {
                     Point p = new Point(cur.x + dx, cur.y + dy);
-                    this.e_uniformity[1 + dx][1 + dy] = this.f_uniformity(prev, next, p, snakelength, points.size());
-                    this.e_curvature[1 + dx][1 + dy] = this.f_curvature(prev, p, next);
-                    this.e_flow[1 + dx][1 + dy] = this.f_gflow(cur, p, flow);
-                    this.e_inertia[1 + dx][1 + dy] = this.f_inertia(cur, p, gradient);
-                    ++dx;
+                    energyUniformity[1 + dx][1 + dy] = this.forceUniformity(prev, p, snakelength, points.size());
+                    energyCurvature[1 + dx][1 + dy] = this.forceCurvature(prev, p, next);
+                    energyFlow[1 + dx][1 + dy] = this.forceGflow(cur, p, flow);
+                    energyInertia[1 + dx][1 + dy] = this.forceInertia(cur, p, gradient);
                 }
-                ++dy;
             }
-            this.normalize(this.e_uniformity);
-            this.normalize(this.e_curvature);
-            this.normalize(this.e_flow);
-            this.normalize(this.e_inertia);
+            normalize(energyUniformity);
+            normalize(energyCurvature);
+            normalize(energyFlow);
+            normalize(energyInertia);
             double emin = Double.MAX_VALUE;
-            double e = 0.0;
             int x = 0;
             int y = 0;
-            int dy2 = -1;
-            while (dy2 <= 1) {
-                int dx = -1;
-                while (dx <= 1) {
-                    e = 0.0;
-                    e += this.alpha * this.e_uniformity[1 + dx][1 + dy2];
-                    e += this.beta * this.e_curvature[1 + dx][1 + dy2];
-                    e += this.gamma * this.e_flow[1 + dx][1 + dy2];
-                    if ((e += this.delta * this.e_inertia[1 + dx][1 + dy2]) < emin) {
+            for (int dy = -1; dy < 2; dy++) {
+                for (int dx = -1; dx < 2; dx++) {
+                    double e = 0.0;
+                    e += alpha * energyUniformity[1 + dx][1 + dy];
+                    e += beta * energyCurvature[1 + dx][1 + dy];
+                    e += gamma * energyFlow[1 + dx][1 + dy];
+                    if ((e += delta * energyInertia[1 + dx][1 + dy]) < emin) {
                         emin = e;
                         x = cur.x + dx;
-                        y = cur.y + dy2;
+                        y = cur.y + dy;
                     }
-                    ++dx;
                 }
-                ++dy2;
             }
             if (x < 1) {
                 x = 1;
@@ -217,43 +220,41 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
                 changed = true;
             }
             newPoints.add(new Point(x, y));
-
         }
-
         if (changed) {
             return newPoints;
         }
-
         return null;
     }
 
+    /**
+     * Normalize the points
+     * 
+     * @param array3x3 
+     */
     private void normalize(double[][] array3x3) {
-        int j;
         double sum = 0.0;
-        int i = 0;
-        while (i < 3) {
-            j = 0;
-            while (j < 3) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 sum += Math.abs(array3x3[i][j]);
-                ++j;
             }
-            ++i;
         }
         if (sum == 0.0) {
             return;
         }
-        i = 0;
-        while (i < 3) {
-            j = 0;
-            while (j < 3) {
-                double[] arrd = array3x3[i];
-                int n = j++;
-                arrd[n] = arrd[n] / sum;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                array3x3[i][j] = array3x3[i][j] / sum;
             }
-            ++i;
         }
     }
 
+    /**
+     * Returns the snake length
+     * 
+     * @param snake
+     * @return double
+     */
     private double getsnakelength(List<Point> snake) {
         double length = 0.0;
         int i = 0;
@@ -266,21 +267,45 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
         return length;
     }
 
-    private double distance2D(Point A, Point B) {
-        int ux = A.x - B.x;
-        int uy = A.y - B.y;
+    /**
+     * Calculate the distance between two points
+     * 
+     * @param a
+     * @param b
+     * @return double
+     */
+    private double distance2D(Point a, Point b) {
+        int ux = a.x - b.x;
+        int uy = a.y - b.y;
         double un = ux * ux + uy * uy;
         return Math.sqrt(un);
     }
-
-    private double f_uniformity(Point prev, Point next, Point p, double snakeLength, int snakeSize) {
+    
+    /**
+     * Calculate the Uniformity force
+     * 
+     * @param prev
+     * @param p
+     * @param snakeLength
+     * @param snakeSize
+     * @return double
+     */
+    private double forceUniformity(Point prev, Point p, double snakeLength, int snakeSize) {
         double un = this.distance2D(prev, p);
         double avg = snakeLength / (double) snakeSize;
         double dun = Math.abs(un - avg);
         return dun * dun;
     }
 
-    private double f_curvature(Point prev, Point p, Point next) {
+    /**
+     * Calculate the Curvature force
+     * 
+     * @param prev
+     * @param p
+     * @param next
+     * @return double
+     */
+    private double forceCurvature(Point prev, Point p, Point next) {
         int ux = p.x - prev.x;
         int uy = p.y - prev.y;
         double un = Math.sqrt(ux * ux + uy * uy);
@@ -292,70 +317,47 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
         }
         double cx = (double) (vx + ux) / (un * vn);
         double cy = (double) (vy + uy) / (un * vn);
-        double cn = cx * cx + cy * cy;
-        return cn;
+        return cx * cx + cy * cy;
     }
 
-    private double f_gflow(Point cur, Point p, int[][] flow) {
+    /**
+     * Calculate de flow force
+     * 
+     * @param cur
+     * @param p
+     * @param flow
+     * @return double
+     */
+    private double forceGflow(Point cur, Point p, int[][] flow) {
         int dcur = flow[cur.x][cur.y];
         int dp = flow[p.x][p.y];
-        double d = dp - dcur;
-        return d;
+        return dp - dcur;
     }
 
-    private double f_inertia(Point cur, Point p, int[][] gradient) {
+    /**
+     * Calculate the inertial force 
+     * 
+     * @param cur
+     * @param p
+     * @param gradient
+     * @return double
+     */
+    private double forceInertia(Point cur, Point p, int[][] gradient) {
         double d = this.distance2D(cur, p);
         double g = gradient[cur.x][cur.y];
-        double e = g * d;
-        return e;
+        return g * d;
     }
 
     private static class ChamferDistance {
 
-        public static final int[][] cheessboard = {
-            {1, 0, 1},
-            {1, 1, 1}};
-
-        public static final int[][] chamfer3 = {
-            {1, 0, 3},
-            {1, 1, 4}};
-
-        public static final int[][] chamfer5 = {
+        private final int[][] chamfer = {
             {1, 0, 5},
             {1, 1, 7},
-            {2, 1, 11}};
-
-        public static final int[][] chamfer7 = {
-            {1, 0, 14},
-            {1, 1, 20},
-            {2, 1, 31},
-            {3, 1, 44}};
-
-        public static final int[][] chamfer13 = {
-            {1, 0, 68},
-            {1, 1, 96},
-            {2, 1, 152},
-            {3, 1, 215},
-            {3, 2, 245},
-            {4, 1, 280},
-            {4, 3, 340},
-            {5, 1, 346},
-            {6, 1, 413}};
-
-        private int[][] chamfer = null;
-        private int normalizer = 0;
+            {2, 1, 11}
+        };
 
         private int width = 0;
         private int height = 0;
-
-        public ChamferDistance() {
-            this(chamfer3);
-        }
-
-        public ChamferDistance(int[][] chamfermask) {
-            chamfer = chamfermask;
-            normalizer = chamfer[0][2];
-        }
 
         private void testAndSet(double[][] output, int x, int y, double newvalue) {
             if ((x < 0) || (x >= width)) {
@@ -393,7 +395,6 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
                             int dx = chamfer[k][0];
                             int dy = chamfer[k][1];
                             int dt = chamfer[k][2];
-
                             testAndSet(output, x + dx, y + dy, v + dt);
                             if (dy != 0) {
                                 testAndSet(output, x - dx, y + dy, v + dt);
@@ -416,7 +417,6 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
                             int dx = chamfer[k][0];
                             int dy = chamfer[k][1];
                             int dt = chamfer[k][2];
-
                             testAndSet(output, x - dx, y - dy, v + dt);
                             if (dy != 0) {
                                 testAndSet(output, x + dx, y - dy, v + dt);
@@ -433,7 +433,7 @@ public class SnakeProcess extends ImageProcess<BinaryImage> {
             }
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
-                    output[x][y] /= normalizer;
+                    output[x][y] /= 0;
                 }
             }
             return output;
